eed47f23e3b75d2d0a8f806faba87136ae20cd87
S Revanth Shalon Raj <revanthshalon@gmail.com>
2022-07-26 21:59:03 +0530

    API Call Update

--- NPI Registry/DTO/HCPDTO.cs
+++ NPI Registry/DTO/HCPDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,61 @@ namespace NPI_Registry.DTO
 {
     internal class HCPDTO
     {
+        [JsonProperty("results")]
+        public List<HCPDetail> HCPDetails { get; set; }
+    }
+    class HCPDetail
+    {
+        [JsonProperty("number")]
+        public long NPI { get; set; }
+        [JsonProperty("basic")]
+        public UserDetail UserDetails { get; set; }
+        [JsonProperty("addresses")]
+        public List<Address> Addresses { get; set; }
+        [JsonProperty("taxonomies")]
+        public List<Taxonomy> Taxonomies { get; set; }
+    }
+    class UserDetail
+    {
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; } = string.Empty;
+        [JsonProperty("last_name")]
+        public string LastName { get; set; } = string.Empty;
+        [JsonProperty("middle_name")]
+        public string MiddleName { get; set; } = string.Empty;
+        [JsonProperty("credential")]
+        public string Credential { get; set; } = string.Empty;
+        [JsonProperty("gender")]
+        public string Gender { get; set; } = string.Empty;
+        [JsonProperty("certification_date")]
+        public DateTime CertificationDate { get; set; }
+    }
+    class Address
+    {
+        [JsonProperty("country_code")]
+        public string CountryCode { get; set; } = string.Empty;
+        [JsonProperty("country_name")]
+        public string CountryName { get; set; } = string.Empty;
+        [JsonProperty("address_1")]
+        public string HouseAddress { get; set; } = string.Empty;
+        [JsonProperty("address_2")]
+        public string StreetAddress { get; set; } = string.Empty;
+        [JsonProperty("city")]
+        public string City { get; set; } = string.Empty;
+        [JsonProperty("state")]
+        public string State { get; set; } = string.Empty;
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; } = string.Empty;
+    }
+    class Taxonomy
+    {
+        [JsonProperty("desc")]
+        public string Description { get; set; } = string.Empty;
+        [JsonProperty("primary")]
+        public bool Primary { get; set; } = false;
+        [JsonProperty("state")]
+        public string State { get; set; } = string.Empty;
+        [JsonProperty("license")]
+        public string License { get; set; } = string.Empty;
     }
 }
--- NPI Registry/NPIRegistry/NPIDetails.cs
+++ NPI Registry/NPIRegistry/NPIDetails.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NPI_Registry.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace NPI_Registry.NPIRegistry
 {
     internal class NPIDetails
     {
+        private const string host = $"https://npiregistry.cms.hhs.gov/api/";
+
+        public static async Task<HCPDTO> GetHCPDetails(int npi)
+        {
+            string urlParams = $@"?number={npi.ToString()}&version=2.1";
+            return await Web.API.RunAsync<HCPDTO>(host, urlParams);
+        }
     }
 }
